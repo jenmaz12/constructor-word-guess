8145534bdfa6094faf62fc092afaf54c84c25cea
jenmaz12 <jenmaz12@gmail.com>
2019-03-20 12:35:03 -0400

    no duplicate play again

--- index.js
+++ index.js
@@ -1,56 +1,69 @@
-const Word = require("./word.js");
-const inquirer = require("inquirer");
-let wordBank = ["Titanic", "Forrest Gump", "A Few Good Men", "Shawshank Redemption", "Remember the Titans", "Harry Potter", "The Wizard of Oz", "The Sound of Music", "Pitch Perfect", "Star Wars", "Rocky", "Wedding Crashers", "The Proposal", "When Harry Met Sally..."];
+const Word = require('./word.js');
+const inquirer = require('inquirer');
+let wordBank = [
+  'Titanic',
+  'Forrest Gump',
+  'A Few Good Men',
+  'Shawshank Redemption',
+  'Remember the Titans',
+  'Harry Potter',
+  'The Wizard of Oz',
+  'The Sound of Music',
+  'Pitch Perfect',
+  'Star Wars',
+  'Rocky',
+  'Wedding Crashers',
+  'The Proposal',
+  'When Harry Met Sally...',
+];
 let currentWord;
 
 function guessing() {
-    inquirer
-        .prompt([
-           {
-            name: "guess",
-            type: "input",
-            message: "Guess a letter!"
-           }
-        ])
-        .then(function(answer){
-            currentWord.checkGuess(answer.guess);
-            console.log(currentWord.stringWord());
+  inquirer
+    .prompt([
+      {
+        name: 'guess',
+        type: 'input',
+        message: 'Guess a letter!',
+      },
+    ])
+    .then(function(answer) {
+      currentWord.checkGuess(answer.guess);
+      console.log(currentWord.stringWord());
 
-            if (currentWord.stringWord().includes("_ ")) {
-                guessing();
-            }
-            else {
-                playAgain();
-            }
-        })
+      if (currentWord.stringWord().includes('_ ')) {
+        guessing();
+      } else {
+        playAgain();
+      }
+    });
 }
 
 function startGame() {
-    currentWord = new Word(wordBank[Math.floor(Math.random() * (wordBank.length - 1))]);
-    console.log(currentWord.stringWord());
-    guessing();    
-};
+  currentWord = new Word(
+    wordBank[Math.floor(Math.random() * (wordBank.length - 1))]
+  );
+  console.log(currentWord.stringWord());
+  guessing();
+}
 
 function playAgain() {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "playAgain",
-                message: "Would you like to play again?",
-                choices: ["Yes", "No"]
-            }
-        ])
-        .then(function(answer) {
-            if (answer.playAgain === "Yes") {
-                startGame();
-                guessing();
-            }
-            else {
-                console.log("Thanks for playing!");
-            }
-        })
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'playAgain',
+        message: 'Would you like to play again?',
+        choices: ['Yes', 'No'],
+      },
+    ])
+    .then(function(answer) {
+      if (answer.playAgain === 'Yes') {
+        startGame();
+      } else {
+        console.log('Thanks for playing!');
+      }
+    });
 }
 
 startGame();
-
